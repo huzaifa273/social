e69e60590a765aa8f5b39329c8f1ce9e66e82bfa
Malik Huzaifa Aawais <malikhuzaifaawais1@gmail.com>
2024-04-27 08:19:21 +0500

    updated cors and vercel.json file

--- server/index.js
+++ server/index.js
@@ -15,7 +15,13 @@ mongoose
     console.log("Connection to Database failed");
   });
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://social-client-chi.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/api/user", userRoute);
 app.use("/api/post", postRoute);
