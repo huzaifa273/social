5044fb9beb7e309f478100ada6e41146371ed2fb
Malik Huzaifa Aawais <malikhuzaifaawais1@gmail.com>
2024-04-27 15:48:09 +0500

    pakage,json

--- server/index.js
+++ server/index.js
@@ -22,19 +22,10 @@ app.use(
     credentials: true,
   })
 );
-app.use(function (req, res, next) {
-  //Enabling CORS
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type"
-  );
-});
 app.use(express.json());
 app.use("/api/user", userRoute);
 app.use("/api/post", postRoute);
-
+const port = process.env.PORT || 5000;
 app.listen(5000, () => {
   console.log("Server is running");
 });
